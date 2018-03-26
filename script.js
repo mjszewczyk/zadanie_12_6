f46e260ceae79ff838b363c432a5d40ff408f729
Magda <mjszewczyk91@gmail.com>
2018-03-26 20:45:48 +0200

    Add index.html, script.js and style.css

---
+++ script.js
@@ -0,0 +1,34 @@
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = $('#countries');
+var sep = ' | ';
+$('#search').click(searchCountries);
+$(function () {
+    $('#country-name').focus();
+});
+
+function searchCountries() {
+    var countryName = $('#country-name').val();
+    if (!countryName.length) countryName = 'Poland';
+    $.ajax({
+        url: url + countryName
+        , method: 'GET'
+        , success: showCountriesList
+        , error: onError
+    });
+}
+
+function showCountriesList(resp) {
+    console.log(resp);
+    countriesList.empty();
+    for (var i = 0; i < resp.length; i++) {
+        var country = resp[i];
+        var row = '<li>' + country.name + sep + country.nativeName + sep + country.capital + sep + country.currencies + '</li>';
+        countriesList.append(row);
+    }
+}
+
+function onError(resp) {
+    countriesList.empty();
+    alert('No such country!');
+    $('#country-name').val('').focus();
+}
